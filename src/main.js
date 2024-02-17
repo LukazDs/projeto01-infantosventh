e1e6fa4e258828bee14012812c7d21bdb9f3916e
Lucas Ds <lukazds2016@gmail.com>
2024-02-16 23:41:15 -0300

    feat: add inventory page

--- src/main.js
+++ src/main.js
@@ -15,7 +15,49 @@ function changePage(element) {
     page.innerHTML = ""
     
     if(optionButton === "Inventário") {
-        page.innerHTML = `${optionButton}`
+        makePageInventory(page)
     }
 
+}
+
+function makePageInventory(page) {
+    page.innerHTML = `
+            <section class="page-inventory">
+                <div class="armor-inventory">
+                    <div class="info-topo">Armas e Armaduras</div>
+                    <div class="information">
+                        <div class="armour">
+                            <p>Garras Longas</p>
+                            <img src="./src/assets/garras-icone.png" alt="garras-icone">
+                        </div>
+                        <div class="armour">
+                            <p>Garras Longas</p>
+                            <img src="./src/assets/garras-icone.png" alt="garras-icone">
+                        </div>
+                    </div>   
+                </div>
+                <div class="images-inventory">
+                    <img src="./src/assets/emblema-guerreiro.png" alt="emblema-guerreiro">
+                    <img src="./src/assets/emblema-mago.png" alt="emblema-guerreiro">
+                    <img src="./src/assets/emblema-bruxo.png" alt="emblema-guerreiro">
+                </div>
+                <div class="itens-inventory">
+                    <div class="info-topo">Itens</div>
+                    <div class="information">
+                        <div class="armour">
+                            <p>Poção de vida</p>
+                            <div class="iten-box-desc">
+                                <p>4</p>
+                            </div>
+                        </div>
+                        <div class="armour">
+                            <p>Poção de vida</p>
+                            <div class="iten-box-desc">
+                                <p>4</p>
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+            </section>
+        `
 }
